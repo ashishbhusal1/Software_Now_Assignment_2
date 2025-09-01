03dd3563c08054a95a85d25ecf984867fb46d721
Nabin Pandey <pandeynabin830@gmail.com>
2025-09-01 12:33:03 +1000

    starting  cipher_tool.py with empty function

---
+++ Question1/cipher_tool.py
@@ -0,0 +1,17 @@
+
+# starting my code and just making empty functions So that i will help further
+
+def encrypt_file():
+    pass
+
+def decrypt_file():
+    pass
+
+def verify():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
